173f11f208d61dc39c658362701b1dd30634d841
gorockgorock <hsyjjk@gmail.com>
2024-02-13 00:38:57 +0900

    오타수정

--- src/main/java/com/sparta/travelnewsfeed/controller/UserController.java
+++ src/main/java/com/sparta/travelnewsfeed/controller/UserController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/travel/user")
+@RequestMapping("/api/travel/users")
 public class UserController {
     private final UserService userService;
 
